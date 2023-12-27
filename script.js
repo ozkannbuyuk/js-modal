3de85447be1fc3e4aef27745530243109bc7a3d1
ozkanbuyuk <ozkanbuyuk@icloud.com>
2023-12-27 16:54:25 +0300

    JavaScript - Modal

---
+++ script.js
@@ -0,0 +1,15 @@
+var modalOpen = document.querySelector("#open");
+var modal = document.querySelector(".modal");
+var modalForm = document.querySelector("#form");
+var modalClose = document.querySelector("#close");
+modalOpen.addEventListener("click", () => {
+  modal.style.display = "flex";
+  setTimeout(() => {
+    modalForm.style.left = "50%";
+  }, 200);
+});
+
+modalClose.addEventListener("click", () => {
+  console.log("clicked close button");
+  modal.style.display = "none";
+});
